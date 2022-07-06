2990de4858d813ae95be08c14c91d5dd11bb9e0c
Recepção 2 <Recepo 2@RECEPO2>
2022-07-06 16:12:08 -0300

    Adicionar arquivos de projeto.

---
+++ Ex5Av/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5Av
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //5 - Fazer um programa para encontrar todos os números pares entre 1 e 100.
+            int contador = 0;
+            while (contador < 100)
+            {
+                contador = contador + 2;
+                Console.WriteLine(contador);
+            }
+            Console.ReadKey();
+        }
+    }
+}
